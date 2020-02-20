084dd46fe250c84edaa3e1ed0757069263a88b5d
Ihor Omelchenko <counter3d@gmail.com>
2020-02-20 11:07:03 +0200

    Add experiment: SEResNeXt

---
+++ bengaliai/experiments/seresnext_aug2/experiment.py
@@ -0,0 +1,149 @@
+"""
+Use DenseNet201 from torchvision pretrained models.
+Data loading from zip file.
+Use albumentation for data augmentation.
+Configuration from json file.
+
+Usage:
+
+from bengaliai.experiments import densenet
+runner, experiment, config = densenet.run()
+"""
+
+import os
+import json
+import datetime
+from collections import OrderedDict
+
+import pandas as pd
+import cv2
+import albumentations
+from albumentations.pytorch.transforms import ToTensorV2
+from sklearn.model_selection import train_test_split
+
+from torchvision import models
+
+from catalyst.dl import ConfigExperiment
+#from catalyst.dl import SupervisedRunner
+from catalyst.dl import SupervisedWandbRunner as SupervisedRunner
+from catalyst.dl import utils
+
+from bengaliai.data.zip_dataset import ZIPImageDataset
+from bengaliai.models.pretrained_classifier import PretrainedModelsBengaliClassifier
+from bengaliai.metrics import HMacroAveragedRecall, AverageMetric
+from bengaliai.data.parquet2zip import parquet_to_images
+from bengaliai.config import *
+from .config import experiment_name, experiment_config
+
+import wandb
+
+SIZE = 224
+ZIP_TRAIN_FILE = f'train{SIZE}.zip'
+ZIP_TEST_FILE = f'test{SIZE}.zip'
+
+
+class Experiment(ConfigExperiment):
+    @staticmethod
+    def get_transforms(stage: str = None, mode: str = None):
+        if mode == 'train':
+            return albumentations.Compose([
+                # blur
+                albumentations.OneOf([
+                    albumentations.Blur((1, 4), p=1.0),
+                    albumentations.GaussianBlur(3, p=1.0),
+                    albumentations.MedianBlur(blur_limit=5, p=1.0),
+                ], p=3/4),
+                # transformations
+                albumentations.ShiftScaleRotate(scale_limit=0.2, rotate_limit=25, border_mode=cv2.BORDER_CONSTANT, value=0, p=1.0),
+                # cut and drop
+                albumentations.OneOf([
+                    albumentations.Cutout(num_holes=10, max_h_size=SIZE//6, max_w_size=SIZE//6, p=1.0),
+                    albumentations.CoarseDropout(max_holes=8, max_height=10, max_width=10, p=1.0),
+                ], p=2/3),
+                # distortion
+                albumentations.OneOf([
+                    albumentations.OpticalDistortion(0.6, p=1.0),
+                    albumentations.GridDistortion(8, 0.06, border_mode=cv2.BORDER_CONSTANT, value=0, p=1.0),
+                    albumentations.ElasticTransform(sigma=10, alpha=1, alpha_affine=10, border_mode=cv2.BORDER_CONSTANT, value=0, p=1.0),
+                ], p=3/4),
+                # add noise
+                albumentations.OneOf([
+                    albumentations.GaussNoise((0, 250), p=1.0),
+                    albumentations.MultiplicativeNoise(p=1.0),
+                ], p=2/3),
+                # common
+                albumentations.Normalize(TRAIN_MEAN, TRAIN_STD),
+                ToTensorV2(),
+            ])
+        elif mode == 'valid':
+            return albumentations.Compose([
+                albumentations.Normalize(TRAIN_MEAN, TRAIN_STD),
+                ToTensorV2(),
+            ])
+        else:
+            raise ValueError('mode is %s' % mode)
+    
+    def get_datasets(self, stage: str, **kwargs):
+        train_df_ = pd.read_csv('train.csv')
+        test_df_ = pd.read_csv('test.csv')
+        class_map_df = pd.read_csv('class_map.csv')
+        sample_sub_df = pd.read_csv('sample_submission.csv')
+        train_df, val_df = train_test_split(train_df_, test_size=VALID_SIZE, random_state=SEED, shuffle=False)
+
+        train_transform = self.get_transforms(stage, 'train')
+        train_dataset = ZIPImageDataset(ZIP_TRAIN_FILE, train_df, output_classes.keys(), train_transform)
+        
+        valid_transform = self.get_transforms(stage, 'valid')
+        valid_dataset = ZIPImageDataset(ZIP_TRAIN_FILE, val_df, output_classes.keys(), transform=valid_transform)
+        
+        return OrderedDict((
+            ('train', train_dataset),
+            ('valid', valid_dataset),
+        ))
+    
+    @staticmethod
+    def _get_model(model_name: str, output_classes: list, pretrained: str):
+        model = PretrainedModelsBengaliClassifier(model_name, output_classes, pretrained)
+        return model
+
+
+def load_config_from_json(filepath: str = __file__):
+    path = os.path.dirname(os.path.abspath(filepath))
+
+    with open(os.path.join(path, 'config.json')) as f:
+        return json.load(f)
+
+
+def run(name: str = None, config: dict = None, device: str = None) -> dict:
+    config = config or experiment_config
+    device = device or utils.get_device()
+    print(f"device: {device}")
+
+    utils.set_global_seed(SEED)
+
+    # inititalize weigths & biases
+    name = name or '_'.join(filter(None, [experiment_name, f"{datetime.datetime.now():%Y-%m-%d-%S}"]))
+
+    # convert parquet ot zip
+    parquet_to_images(TRAIN, ZIP_TRAIN_FILE, SIZE)
+    parquet_to_images(TEST, ZIP_TEST_FILE, SIZE)
+
+    # run experiment
+    runner = SupervisedRunner(
+        device=device,
+        input_key="images",
+        output_key=["logit_" + c for c in output_classes.keys()],
+        input_target_key=list(output_classes.keys()),)
+    experiment = Experiment(config)
+    runner.run_experiment(experiment)
+
+    return {
+        'runner': runner,
+        'experiment': experiment,
+        'config': config,
+    }
+
+
+if __name__ == '__main__':
+    run()
+
--- bengaliai/models/pretrained_classifier.py
+++ bengaliai/models/pretrained_classifier.py
@@ -4,11 +4,12 @@ import pretrainedmodels
 from .mutlilinear import *
 
 
-class PretrainedModelsBengaliClassifier:
-    def __init__(self, backbone, output_classes, pretrained='imagenet'):
+class PretrainedModelsBengaliClassifier(nn.Module):
+    def __init__(self, backbone, output_classes, pretrained='imagenet', one_channel: bool = True):
         super().__init__()
         
         self.output_classes = output_classes
+        self.one_channel = one_channel
         
         if callable(backbone):
             self.backbone = backbone(pretrained)
@@ -16,7 +17,12 @@ class PretrainedModelsBengaliClassifier:
             self.backbone = getattr(pretrainedmodels, backbone)(pretrained=pretrained)
             
         dim = self.backbone.last_linear.in_features
-        self.backbone.classifier = nn.Identity()
+        self.backbone.last_linear = nn.Identity()
+        
+        if one_channel:
+            conv0 = next((l for l in self.backbone.modules() if isinstance(l, nn.Conv2d)))
+            conv0.weight = nn.Parameter(conv0.weight.mean(1, keepdim=True))
+            conv0.in_channels = 1
         
         self.classifier = MultiLabelLinearClassfier(dim, output_classes)
         
@@ -25,10 +31,10 @@ class PretrainedModelsBengaliClassifier:
         return next(self.parameters()).device
         
     def forward(self, x):
-        x = x.repeat(1, 3, 1, 1)
+        if not self.one_channel:
+            x = x.repeat(1, 3, 1, 1)
         
         x = self.backbone(x)
-        x = torch.sum(x, dim=(-1, -2))
         x = self.classifier(x)
         
         return x
